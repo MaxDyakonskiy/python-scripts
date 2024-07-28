11e5ae0d121086ad928cba5aaf661d7d3f8f613f
Maxim Dyakonskiy <dyakonskiymaksim@gmail.com>
2024-07-28 13:16:26 +0300

    Script for unpacking deb packages with nested tar.gz archives -- ready

--- unpack-debs-and-nested-targzs.py
+++ unpack-debs-and-nested-targzs.py
@@ -11,15 +11,17 @@
 import os
 import os.path
 import re
-import subprocess
 import shutil
+import subprocess
+import sys
 import tarfile
+import traceback
 
 
 def path_to_deb_pkgs():
     global working_dir
     path = input("Type in a path to a directory with deb-packages: ")
-    
+
     match list(path):
         case ("/", *_) if path is not None:
             working_dir = path
@@ -32,16 +34,16 @@ def path_to_deb_pkgs():
 
     os.chdir(working_dir)
 
+
 def unpack_deb_pkgs():
     global deb_pkgs, dirs_for_deb_pkg
-    
+
     re_deb = re.compile(r"\.deb$")
     deb_pkgs = list(filter(re_deb.search, os.listdir()))
     dirs_for_deb_pkg = [str(i).removesuffix(".deb") for i in deb_pkgs]
-    
+
     print("\n\nThe cwd contains " + str(len(deb_pkgs)) + " debian packages.\n")
-    
-    
+
     # Make direcrories
     for package in deb_pkgs:
         for directory in dirs_for_deb_pkg:
@@ -49,44 +51,42 @@ def unpack_deb_pkgs():
                 if directory not in os.listdir():
                     os.mkdir(directory)
                     print(
-                        "Made a directory where "
-                         + str(package)
-                         + " will be unpacked"
+                            "Made a directory where "
+                            + str(package)
+                            + " will be unpacked"
                     )
                 if any(os.scandir(directory)) is False:
                     shutil.move(package, directory)
                     subprocess.run(
-                        [shutil.which("ar"), "xv", "--output", directory, 
-                        directory + "/" + package],
+                        [
+                            shutil.which("ar"), "xv", "--output", directory,
+                            directory + "/" + package
+                        ],
                         check=True
                     )
                     os.remove(directory + "/" + package)
-                    
+
 
 def unpack_archives_in_unpacked_debs():
     re_tar_gz = re.compile(r"\.tar\.gz$")
 
     for directory, _, files in os.walk(working_dir):
         for file in filter(re_tar_gz.search, files):
-            try: 
+            try:
                 tarfile.open(
-                    name=os.path.join(directory, file), 
+                    name=os.path.join(directory, file),
                     mode='r:gz'
-                    ).extractall(path=directory, filter='data')
-            except: 
-                print("Is it a tar.gz file?")
-            else: 
+                    ).extractall(path=directory, filter='tar')
+            except Exception:
+                print("Short exception:")
+                print(sys.exc_info())
+
+                print("\nLong exception:")
+                traceback.print_exc()
+            else:
                 os.remove(os.path.join(directory, file))
 
 
 path_to_deb_pkgs()
 unpack_deb_pkgs()
 unpack_archives_in_unpacked_debs()
-
-    # https://github.com/avinashkranjan/Amazing-Python-Scripts/blob/main/Audio%20to%20Text%20Convertor/code.py#L28 -- пример скрипта
-    
-    # https://stackoverflow.com/questions/3702675/catch-and-print-full-python-exception-traceback-without-halting-exiting-the-prog
-
-# generators: https://stackoverflow.com/questions/1756096/understanding-generators-in-python
-# os.listdir -- возвращает list
-# os.scandir -- возвращает iterator of DirEntry   
